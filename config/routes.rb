73f091461a19ac0510c932ff9a29393e3a51557c
Ross Munn <ross.munn888@gmail.com>
2021-11-23 17:34:39 +0000

    Navbar and database testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  #get 'home/index'
+  
+  # /pokemon/:id/image.png
+  resources :pokemons do
+    member do
+      get :image
+    end
+  end
+
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/contact'
+ 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\1-Bulbasaur.png', 'rb')
+Pokemon.create!(name: 'bulbasaur', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\2-Ivysaur.png', 'rb')
+Pokemon.create!(name: 'Ivysaur', image: file.read)
