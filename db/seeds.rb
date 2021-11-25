dca0515969fdf67314b41ba83934c4f9f8fbf9a4
Ross Munn <ross.munn888@gmail.com>
2021-11-25 00:40:49 +0000

    homepage css and contact page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\1-Bulbasaur.png', 'rb')
-Pokemon.create!(name: 'bulbasaur', image: file.read)
+Pokemon.create!(name: 'Bulbasaur', image: file.read)
 file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\2-Ivysaur.png', 'rb')
 Pokemon.create!(name: 'Ivysaur', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\3-Venusaur.png', 'rb')
+Pokemon.create!(name: 'Venusaur', image: file.read)
+
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\4-Charmander.png', 'rb')
+Pokemon.create!(name: 'Charmander', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\5-Charmeleon.png', 'rb')
+Pokemon.create!(name: 'Charmeleon', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\6-Charizard.png', 'rb')
+Pokemon.create!(name: 'Charizard', image: file.read)
+
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\7-Squirtle.png', 'rb')
+Pokemon.create!(name: 'Squirtle', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\8-Wartortle.png', 'rb')
+Pokemon.create!(name: 'Wartortle', image: file.read)
+file  = File.open('C:\RubyRails\WebCW\app\assets\images\pokemon\9-Blastoise.png', 'rb')
+Pokemon.create!(name: 'Blastoise', image: file.read)
